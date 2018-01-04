5dd44d774df7d906ddd68f3f02f3be1c3144696b
Nicolas Turner <nicolasrturner@gmail.com>
2018-01-03 17:11:15 -1000

    created ViewModel

--- js/app.js
+++ js/app.js
@@ -146,15 +146,26 @@ function initMap() {
         ];
 
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7413549, lng: -73.9980244},
-          zoom: 13,
+          center: {lat: 21.280945, lng: -157.813963},
+          zoom: 14,
           styles: styles,
           mapTypeControl: false
         });
+
+
 }
 
 //Error handling for Google Maps
 function mapError() {
-	document.getElementById("map").innerHTML = "<h2> Sorry, this is not the map you are looking for. </h2>";
-	console.log("This is not the map you are looking for.");
+	document.getElementById("map").innerHTML = "<h2> Sorry, Google Maps could not load </h2>";
+	console.log("Sorry, Google Maps had a problem loading");
 };
+
+var ViewModel = function() {
+	var self = this;
+	// search bar
+	this.search = ko.observable('');
+
+};
+
+ko.applyBindings(new ViewModel());
